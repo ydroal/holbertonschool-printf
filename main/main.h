2addb528680fd378df10fe5e7a776d5afbde5121
annlaure02 <alaure.g02@gmail.com>
2022-11-07 07:44:30 -0800

    update main.h

---
+++ main/main.h
@@ -0,0 +1,20 @@
+#ifndef _PRINTF_H_
+#define _PRINTF_H_
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+/**
+ * struct form -  list of types of arguments
+ * @c: char
+ * @f: function's pointer
+ * Description: Longer description
+ */
+typedef struct print_type
+{
+	char *c;
+	void (*f)(va_list ap);
+} tab_type;
+
+#endif
