417d0d871f5ce25a77494b84b234835ac75a1c13
ydroal <5617@holbertonstudents.com>
2022-11-08 07:03:35 -0800

    update: main.h add: all_print.c

--- main.h
+++ main.h
@@ -3,18 +3,24 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int _printf(const char *format, ...);
-
 /**
  * struct form -  list of types of arguments
- * @c: char
+ *
+ * @c: conversion specifiers 
  * @f: function's pointer
+ *
  * Description: Longer description
  */
 typedef struct print_type
 {
 	char *c;
-	void (*f)(va_list ap; int len);
+	int (*f)(va_list ap);
 } tab_type;
+
+int _printf(const char *format, ...);
+int _strlen(char *);
+int print_char(va_list ap);
+int print_string(va_list ap);
+int print_percent(va_list ap);
 
 #endif
