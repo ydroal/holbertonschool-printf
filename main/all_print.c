3c4dbed76f849f54444ca1da75f966696de133ef
ydroal <5617@holbertonstudents.com>
2022-11-08 02:19:48 -0800

    add: all_print.c

---
+++ main/all_print.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int print_char(va_list ap)
+{
+	int c;
+	c = va_arg(ap, int);
+	write(1, &c, 1);
+	return (1);
+}
+
+int print_string(va_list ap)
+{
+	char *str;
+	int len;
+	str = va_arg(ap, char*);
+	len = _strlen(str);
+	return (write(1, str, len));
+}
+
+int print_percent(va_list ap)
+{
+	char *str;
+	int len;
+	str = va_arg(ap, char*);
+	len = _strlen(str);
+	write(1, '%', 1);
+	write(1, str, len);
+	return (len + 1);
+}
