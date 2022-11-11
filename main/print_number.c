637432d36634307cd6f1f50862e7fe6c9c9aaa60
ydroal <5617@holbertonstudents.com>
2022-11-11 01:12:19 -0800

    update: print_number.c

--- main/print_number.c
+++ main/print_number.c
@@ -11,17 +11,24 @@ int print_number(va_list ap)
 {
 	int n;
 	int len = 0;
+	int div = 1;
 
 	n = va_arg(ap, int);
-	if ( n < 0)
+	if (n < 0)
 	{
 		len += _putchar('-');
 		n *= -1;
 	}
-	while (n >= 0 && n <= 9)
+
+	while ((n / div) > 9)
+		div *= 10;
+
+	while (div != 0)
 	{
-		len += _putchar((n / 10) + '0');
-		len += _putchar((n % 10) + '0');
+		len += _putchar((n / div) + '0');
+		n %= div;
+		div /= 10;
 	}
 	return (len);
+
 }
